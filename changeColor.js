b880bde7060a6b979341e030d3f604869df97330
Reed Schimmel <reeder960@gmail.com>
2020-03-29 00:52:23 -0500

    execercise 4 complete

--- changeColor.js
+++ changeColor.js
@@ -1,19 +1,26 @@
-
 const getValues = (prefix) => ['Red', 'Blue', 'Green']
   .map(color => {
     const value = document.getElementById(`${prefix}${color}`).value;
     console.log(value)
 
-    if ((value / 255) <= 1 && (value / 255) >= 0) return value;
+    if (value < 256 && value >= 0) return value;
     alert(`Value in ${color} section of ${prefix} is invalid!`);
     return 0;
   });
 
 const changeColor = () => {
   const paragraph = document.getElementById("mainText");
-  let [red, green, blue] = getValues('background');
-  paragraph.style.backgroundColor = `rgb(${red},${green},${blue})`;
-  paragraph.style["border-width"] = document.getElementById("borderWidth").value;
-
+  
+  // Border styling
+  let [red, green, blue] = getValues('border');
+  paragraph.style = `
+  border-style: solid;
+  border-width: ${document.getElementById("borderWidth").value};
+  border-color: rgb(${red},${green},${blue});
+  `;
+  
+  // Background styling
   ([red, green, blue] = getValues('background'));
+  paragraph.style.backgroundColor = `rgb(${red},${green},${blue})`;
+
 }
